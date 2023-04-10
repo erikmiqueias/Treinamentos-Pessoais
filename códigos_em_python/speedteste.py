5af781fc4923511b1ea6681eeea012412553367b
erikmiqueias <erikmiqueiaspereira@outlook.com>
2023-04-09 23:39:43 -0400

    Teste de Velocidade de Internet

---
+++ códigos_em_python/speedteste.py
@@ -0,0 +1,15 @@
+# Testando velocidade de Internet com python
+import speedtest
+from time import sleep
+
+spt = speedtest.Speedtest()
+spt.get_best_server()
+
+spt_ping = spt.results.ping
+spt_download = round(spt.download() / 1000 / 1000, 1)
+spt_upload = round(spt.upload() / 1000 / 1000, 1)
+
+print(f"Velocidade de Download {spt_download}")
+print(f"Velocidae de Upload {spt_upload}")
+print(f"Nível de Ping {spt_ping}")
+
