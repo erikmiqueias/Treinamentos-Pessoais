573aa8b972e1cc04c1f58f2cc1f58fdc7de13a0d
erikmiqueias <erikmiqueiaspereira@outlook.com>
2023-05-03 13:51:43 -0400

    Conversor de Bases Numéricas

--- códigos_em_python/conversordebasesnúmericas.py
+++ códigos_em_python/conversordebasesnúmericas.py
@@ -1,17 +1,60 @@
 # Conversor de Bases Numéricas
-n1 = int(input("Digite um número inteiro: "))
-print('''Escolha uma das bases para conversão:
-[ 1 ] converter para BINÁRIO
-[ 2 ] converter para OCTAL
-[ 3 ] converter para HEXADECIMAL''')
-opcao = int(input("Sua opção: "))
-if opcao == 1:
-    print("{} convertido para BINÁRIO é igual a {}".format(n1, bin(n1)[2:]))
-elif opcao == 2:
-    print("{} convertido para OCTAL é igual a {}".format(n1, oct(n1)[2:]))    
+from time import sleep
+print("-=-" * 10)
+print("Bem Vindo ao conversor de bases numéricas!")
+print("-=-" * 10)
+sleep(2)
 
-elif opcao == 3:
-    print("{} convertido para HEXADECIMAL é igual a {}".format(n1, hex(n1)[2:]))
-    
-else:
-    print("Digite uma opção válida entre 1 e 3!")
+def conv():
+    while True:
+        n1 = int(input("Digite um número inteiro: "))
+        print('''Escolha uma das bases para conversão:
+        [ 1 ] converter para BINÁRIO
+        [ 2 ] converter para OCTAL
+        [ 3 ] converter para HEXADECIMAL''')
+        opcao = int(input("Sua opção: "))
+        if opcao == 1:
+            print("{} convertido para BINÁRIO é igual a {}".format(n1, bin(n1)[2:]))
+            op = str(input("Deseja continuar a converter S/N? ")).upper().capitalize()
+            if op == 'S':
+                print("Retornando as opções...")
+                sleep(2)
+                return(conv())
+            elif op == 'N':
+                print("Saindo...")
+                sleep(2)  
+                break
+        elif opcao == 2:
+            print("{} convertido para OCTAL é igual a {}".format(n1, oct(n1)[2:]))
+            op = str(input("Deseja continuar a converter S/N? ")).upper().capitalize()
+            if op == 'S':
+                print("Retornando as opções...")
+                sleep(2)
+                return(conv()) 
+            elif op == 'N':
+                print("Saindo...")
+                sleep(2)   
+                break
+        elif opcao == 3:
+            print("{} convertido para HEXADECIMAL é igual a {}".format(n1, hex(n1)[2:]))
+            op = str(input("Deseja continuar a converter S/N? ")).upper().capitalize()
+            if op == 'S':
+                print("Retornando as opções...")
+                sleep(2)
+                return(conv())
+            elif op == 'N':
+                print("Saindo...")  
+                sleep(2)    
+                break
+        else:
+            print("Digite uma opção válida entre 1 e 3!")
+            op = str(input("Deseja continuar a converter S/N? ")).upper().capitalize()
+            if op == 'S':
+                print("Retornando as opções...Lembre de escolher uma opção válida!")
+                sleep(2)
+                return(conv())
+            elif op == 'N':
+                print("Saindo...")
+                sleep(2) 
+                break
+conv()
