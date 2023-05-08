673f460545bc97dabfae4ac21aee370f9658581b
erikmiqueias <erikmiqueiaspereira@outlook.com>
2023-05-08 14:10:53 -0400

    Teste de Velocidade com Python

--- códigos_em_python/speedteste.py
+++ códigos_em_python/speedteste.py
@@ -12,4 +12,3 @@ spt_upload = round(spt.upload() / 1000 / 1000, 1)
 print(f"Velocidade de Download {spt_download}")
 print(f"Velocidae de Upload {spt_upload}")
 print(f"Nível de Ping {spt_ping:.2f}")
-
